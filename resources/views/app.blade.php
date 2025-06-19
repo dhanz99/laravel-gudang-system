ce9371933dfdbb5e9c54430dfdb1f6ff3ffe762a
Hirdhan Aghnia <hirdhanaghnia@Hirdhans-MacBook-Air.local>
2025-06-19 17:33:14 +0700

    Initial commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+       <title>AGHNIA HQ STORE</title>
+
+    </head>
+    <body class="font-sans antialiased">
+        <h1>COBA DULU</h1>
+    </body>
+</html>
